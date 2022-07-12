e0e42f29ea5c7988462be752b3e70ca1035e6f73
lishiwen <lishiwen@wclound.cn>
2022-07-12 12:40:05 +0800

    feat:types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,14 +1,69 @@
-
-export function isObject(value: unknown) {
+// TODO: 使用简单工厂模式和策略模式改写下方⬇️代码
+export function isObject(value: any) {
     return createTypeInspectorSimpleFactory(value, "Object");
 }
-export function isFunction(value: unknown) {
+export function isFunction(value: any) {
     return createTypeInspectorSimpleFactory(value, "Function")
 }
-export function isArray(value: unknown) {
+export function isArray(value: any) {
     return createTypeInspectorSimpleFactory(value, "Array");
 }
+export function isString(value: any): boolean {
+    return createTypeInspectorSimpleFactory(value, "String");
+}
+export function isNumber(value: any): boolean {
+    return createTypeInspectorSimpleFactory(value, "Number");
+}
 
-function createTypeInspectorSimpleFactory(value: unknown, typeOfExpectation:string) {
+
+/**
+ * isMeaningful
+ *
+ * @param value: {unknown}
+ * @param meaningfulOptions?:
+    * @option allowEmptyString?: {boolean}
+    * @option allowEmptyNumber?: {boolean}; // 0
+    * @option allowMinus?: {boolean}; // -1
+    * @option validator?: {(value, meaningfulOptions) => boolean}; // 可用于自定义检验
+ * */
+export interface MeaningfulOptions {
+    allowEmptyString?: boolean;
+    allowEmptyNumber?: boolean;
+    allowMinus?: boolean;
+    validator?: (value: any, meaningfulOptions: MeaningfulOptions) => boolean;
+}
+export function isMeaningful(value: any, meaningfulOptions: MeaningfulOptions = {
+    allowEmptyNumber: false,
+    allowEmptyString: false,
+    allowMinus: true,
+}): boolean {
+    if(isString(value) && isEmptyString(value) && meaningfulOptions.allowEmptyString) return true;
+    if(isNumber(value) && isEmptyNumber(value) && meaningfulOptions.allowEmptyNumber) return true;
+
+    return !!value;
+}
+
+
+export function isEmptyString(value: any):boolean {
+    return value === "";
+}
+export function isEmptyNumber(value: any):boolean {
+    return value === 0;
+}
+
+// isPromise
+// ref: https://262.ecma-international.org/6.0/#sec-promise.then
+export function isPromise(valueOrPromiseInstance: any, strict = true): boolean {
+    if(!isMeaningful(valueOrPromiseInstance)) return false;
+    const isStringPromise = createTypeInspectorSimpleFactory(valueOrPromiseInstance, "Promise");
+    const isThenable = isThenabled(valueOrPromiseInstance);
+    return strict && isStringPromise && isThenable;
+}
+
+export function isThenabled(valueOrPromiseInstance: any): boolean {
+    return isMeaningful(valueOrPromiseInstance) && isFunction(valueOrPromiseInstance.then);
+}
+
+function createTypeInspectorSimpleFactory(value: any, typeOfExpectation:string) {
     return Object.prototype.toString.call(value) === `[object ${typeOfExpectation}]`;
 }
